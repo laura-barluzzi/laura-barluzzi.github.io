e1f0885714132e5319881dafc430895dae51414d
Laura Barluzzi <laura@koalacoder.com>
2017-02-10 16:01:20 -0800

    reduced sized of thumbnails

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+function checkDevice(height, width) {
+  var small = [800, 1000];
+  var heightIsLower = height < small[0];
+  var widthIsLower = width < small[1];
+  return (heightIsLower && widthIsLower) ? small : [0, 0];
+}
 
-$(document).ready(function() {           
+function setUpFullpage(screenSize) {
   $('#fullpage').fullpage({
     anchors:['home-page', 'coding',"art","NGOs","about-contact"],
     menu:"#myNavbar",
-    scrollBar: true
+    scrollBar: true,
+    responsiveHeight: screenSize[0],
+    responsiveWidth: screenSize[1],
   });
+}
 
+
+$(document).ready(function() { 
   
+  var heightScreen = $(window).height(); 
+  var widthScreen = $(window).width();
+  console.log(heightScreen + " width = " + widthScreen);
+     
+  setUpFullpage(checkDevice(heightScreen, widthScreen));      
+
 //So the Mobile Nav Hides When a Link is Clicked
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
